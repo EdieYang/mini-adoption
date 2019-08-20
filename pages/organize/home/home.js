16cc4ce28a6bb8af9a48b93227be7e0d3d58246f
EdieYang <haojie_look@163.com>
2019-08-21 06:55:46 +0800

    更新bug

--- pages/organize/home/home.js
+++ pages/organize/home/home.js
@@ -71,6 +71,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    var that=this
     orgId = options.orgId
     if (options.orgId == '' || typeof options.orgId == 'undefined' || options.orgId == null) {
       orgId = options.scene
